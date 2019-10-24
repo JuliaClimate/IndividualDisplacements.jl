b6ae7f533db59b3de0726591ba2eb951d26db5c0
gaelforget <gforget@mit.edu>
2019-10-24 09:46:51 -0400

    add VelComp method for du::Array{Float64,2}, use grid.ioSize in place of hard-coded nx&ny, mv division by dx to called, improve md cells, + couple other improvements
    Former-commit-id: 2cb29503cb49cffd567d1f3eda0fd51dcda3ef83

--- examples/ex_2_test.jl
+++ examples/ex_2_test.jl
@@ -13,15 +13,17 @@
 #     name: julia-1.1
 # ---
 
-# ## This notebook
+# # This notebook
 #
-# A simplified version of `ex2_more.jl` that can be used for testing without downloading anything.
+# A simplified version of `ex2_more.jl` used in `runtests.jl` (avoids downloading binary data).
 #
 # _Notes:_ For documentation see <https://gaelforget.github.io/MeshArrays.jl/stable/>, <https://docs.juliadiffeq.org/latest/solvers/ode_solve.html> and <https://en.wikipedia.org/wiki/Displacement_(vector)>
 
-# # 1) Get gridded variables via MeshArrays.jl
+# ## 1. import software and `pkg/flt` trajectories
 
 using IndividualDisplacements, MeshArrays, Plots, DifferentialEquations
+
+# ## 2. Define gridded variables as `MeshArray`s
 
 # Put grid variables in a dictionary:
 
@@ -46,17 +48,22 @@ t1=18001.0*3600.0
 u0=-(YG.-YC[1][40,21])/2000000.; u1=u0
 v0=(XG.-XC[1][40,21])/2000000.; v1=v0
 
+u0=u0./GridVariables["dx"]
+u1=u1./GridVariables["dx"]
+v0=v0./GridVariables["dx"]
+v1=v1./GridVariables["dx"]
+
 uvt = Dict("u0" => u0, "u1" => u1, "v0" => v0, "v1" => v1, "t0" => t0, "t1" => t1)
 uvetc=merge(uvt,GridVariables);
 # -
 
-# Initial Conditions etc:
+# ## 3. Define initial condition and time period
 
-uInit=[200000.0;0.0]
+uInit=[200000.0;0.0]./uvetc["dx"]
 nSteps=3000-2
 du=fill(0.0,2);
 
-# ## Solve for position time series using DifferentialEquations.jl
+# ## 4. solve through time using DifferentialEquations.jl
 
 using DifferentialEquations
 tspan = (0.0,nSteps*3600.0)
@@ -64,11 +71,16 @@ prob = ODEProblem(IndividualDisplacements.VelComp,uInit,tspan,uvetc)
 sol = solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
 sol[1:4]
 
+# ## 5. visualize trajectory
+
 using Plots
-Plots.plot(sol[1,:],sol[2,:],linewidth=5,title="Using Recomputed Velocities",
+Plots.plot(sol[1,:],sol[2,:],linewidth=5,title="Solid body rotation example",
      xaxis="lon",yaxis="lat",label="Julia Solution") # legend=false
-# Check results:
+
+# ## 6. check against reference result
 
 sol[1,end],sol[2,end]
-isapprox(sol[1,end],117237.0; atol=1.)
-isapprox(sol[2,end],40448.0; atol=1.)
+isapprox(sol[1,end],117237.0./uvetc["dx"]; atol=1.)
+isapprox(sol[2,end],40448.0./uvetc["dx"]; atol=1.)
+
+
