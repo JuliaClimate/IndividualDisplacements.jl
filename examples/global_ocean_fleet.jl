eee8a7f203d15484445e062ab9c15a15738e9c5f
gaelforget <gforget@mit.edu>
2019-11-08 04:34:41 -0500

    add dMax to parameter list in `PlotBasic` + cleanup `global_ocean_fleet`
    Former-commit-id: a6e63a3007a40b84f015b6ac461d06a71594355a

--- examples/global_ocean_fleet.jl
+++ examples/global_ocean_fleet.jl
@@ -63,7 +63,7 @@ v0=v; v1=v;
 # Put velocity fields and time range in a dictionary.
 
 # +
-t0=0.0; t1=86400*366*10.0; dt=3600;
+t0=0.0; t1=86400*366*2.0; dt=3600;
 
 u0=u0./GridVariables["DXC"]
 u1=u1./GridVariables["DXC"]
@@ -90,67 +90,10 @@ msk=Dict("mskW" => mskW, "mskS" => mskS)
 uvetc=merge(uvetc,msk);
 # -
 
-# ## 3. Visualize  gridded variables
-
-#PyPlot.contourf(GridVariables["XG"].f[1], GridVariables["YC"].f[1], mskS.f[1].*v0.f[1])
-#colorbar()
-heatmap(v0.f[1])
-#heatmap(mskS.f[1].*v0.f[1])
-#PyPlot.contour(GridVariables["XG"].f[1], GridVariables["YC"].f[1], mskS.f[1].*v0.f[1])
-
-# ## 4. Recompute displacements from gridded flow fields
-
-# Initialize individual locations and define method aliases.
-
-# +
-uInit=[180.0,40.0] #uInit=[tmp[1,:lon];tmp[1,:lat]]./uvetc["dx"]
-nSteps=Int32(uvt["t1"]/uvt["dt"]) #nSteps=Int32(tmp[end,:time]/3600)-2
-du=fill(0.0,2);
+# ## 3. Compute trajectories from gridded flow fields
 
 comp_vel=IndividualDisplacements.VelComp
 get_vel=IndividualDisplacements.VelCopy
-# -
-
-# Inspect how `comp_vel` behaves.
-
-# +
-if false 
-    comp_vel(du,uInit,uvetc,0.0); show(du)
-    tmpdu=[uvt["u0"][1][uInit[1]+1,uInit[2]+1] uvt["v0"][1][uInit[1]+1,uInit[2]+1]]; show(tmpdu)
-end
-
-ii=uInit[1]-3:0.1:uInit[1]+3
-jj=uInit[2]-3:0.1:uInit[2]+3
-tmpu=zeros(size(ii))
-tmpv=zeros(size(ii))
-for i in eachindex(ii)
-    comp_vel(du,[ii[i];jj[i]],uvetc,0.0)
-    tmpu[i],tmpv[i]=du
-end
-#tmp=zeros(10,1)
-#for comp_vel(du,[180.1;40.1],uvetc,0.0)
-Plots.plot(tmpu)
-Plots.plot!(tmpv)
-# -
-
-# ## 5. Solve through time using `DifferentialEquations.jl`
-
-using DifferentialEquations
-tspan = (0.0,nSteps*3600.0)
-#prob = ODEProblem(get_vel,uInit,tspan,tmp)
-prob = ODEProblem(comp_vel,uInit,tspan,uvetc)
-sol = solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
-sol[1:4]
-
-#Plots.plot(sol[1,:],sol[2,:])
-sol[:,end-4:end]
-
-Plots.plot(sol[1,2:end]-sol[1,1:end-1])
-Plots.plot!(sol[2,2:end]-sol[2,1:end-1])
-size(sol)
-
-Plots.plot(sol[1,:],sol[2,:])
-
 
 # +
 ii1=0:5:360; ii2=20:2:150;
@@ -167,9 +110,12 @@ comp_vel(du,uInitS,uvetc,0.0)
 du
 # -
 
+tspan = (0.0,uvt["t1"]-uvt["t0"])
 prob = ODEProblem(comp_vel,uInitS,tspan,uvetc)
 sol = solve(prob,Tsit5(),reltol=1e-4,abstol=1e-4)
 size(sol)
+
+# ## 4. Store trajectories in a `DataFrame` and plot
 
 ID=collect(1:size(sol,2))*ones(1,size(sol,3))
 lon=mod.(sol[1,:,:],360)
@@ -177,26 +123,6 @@ lat=mod.(sol[2,:,:],180)
 df = DataFrame(ID=Int.(ID[:]), lon=lon[:], lat=lat[:])
 size(df)
 
-function PlotGlobal(df::DataFrame,nn::Integer)
-   IDs = randperm(maximum(df.ID))
-   COs=["w" "y" "g" "k"]
-   dMax=90.
-
-   for ii=1:nn
-      tmp=df[df.ID .== IDs[ii], :]
-      if dMax > 0.
-         d=abs.(diff(tmp[!,:lon]))
-         jj=findall(d .> dMax)
-         tmp[jj,:lon].=NaN; tmp[jj,:lat].=NaN
-         d=abs.(diff(tmp[!,:lat]))
-         jj=findall(d .> dMax)
-         tmp[jj,:lon].=NaN; tmp[jj,:lat].=NaN
-      end
-      CO=COs[mod(ii,4)+1]
-      PyPlot.plot(tmp[!,:lon],tmp[!,:lat],color=CO,linewidth=0.3)
-   end
-end
-
-PyPlot.figure(); PlotGlobal(df,size(sol,2))
+PyPlot.figure(); PlotBasic(df,size(sol,2),90.0)
 
 
