0257f477ed4addb5ec9c0759834353c948330026
gaelforget <gforget@mit.edu>
2020-02-10 20:13:55 -0500

    cleanup examples 1 & 2

--- src/IndividualDisplacements.jl
+++ src/IndividualDisplacements.jl
@@ -2,15 +2,16 @@ module IndividualDisplacements
 
 greet() = print("Get ready for IndividualDisplacements!")
 
+using MeshArrays, OrdinaryDiffEq, StatsBase, DataFrames, Random
+
 include("ReadIndDisp.jl")
 include("VelocityIndDisp.jl")
 include("examples.jl")
 
-export VelComp, VelComp!, VelCopy, ReadGriddedFields, ReadDisplacements
+export VelComp, VelComp!, VelCopy, ReadDisplacements
 
 #include("plot_pyplot.jl")
 #include("plot_makie.jl")
-
 #export PlotBasic, PlotMapProj, PlotMakie
 
 end # module
