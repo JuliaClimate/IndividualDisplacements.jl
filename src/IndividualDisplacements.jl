d6636e12c595460874b4dcd0e01ceebad0087ebf
gaelforget <gforget@mit.edu>
2020-05-02 07:04:36 -0400

    move helper functions from notebooks to pkg
    Former-commit-id: a96d9d5f3d678957d8e1c302576129c948f747b1 [formerly 253997c4fd9b37f55ff9b12c6ea7f755eb3170bc]
    Former-commit-id: 8caac77d92516ef488c6929c8d3e57d8598fdf1b
    Former-commit-id: 85cbd8b85ab17e9581b4d7fb2a8415d910d362b9

--- src/IndividualDisplacements.jl
+++ src/IndividualDisplacements.jl
@@ -8,8 +8,11 @@ include("compute.jl")
 include("read.jl")
 include("examples.jl")
 include("update_locations.jl")
+include("data_wrangling.jl")
 
 export VelComp!, VelComp, VelCopy, ReadDisplacements
+export initialize_locations, read_uv_etc, postprocess_ODESolution
+
 
 #include("plot_pyplot.jl")
 #include("plot_makie.jl")
