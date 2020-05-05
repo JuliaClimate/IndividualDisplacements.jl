803dc446a5067a671bfe3342105979f48ec1c3e7
gaelforget <gforget@mit.edu>
2020-05-05 02:24:13 -0400

    update deps, PeriodicChannel_fleet, example2fleet, & add example3_setup
    Former-commit-id: 6c4b7de033145f6886baf5a802757e928d9855f5

--- examples/example2fleet.jl
+++ examples/example2fleet.jl
@@ -16,62 +16,58 @@
 
 # # This notebook
 #
-# _Notes:_ For documentation see <https://gaelforget.github.io/MeshArrays.jl/stable/>, <https://docs.juliadiffeq.org/latest/solvers/ode_solve.html> and <https://en.wikipedia.org/wiki/Displacement_(vector)>
+# _Notes:_ For more documentation see <https://docs.juliadiffeq.org/latest/solvers/ode_solve.html> and <https://en.wikipedia.org/wiki/Displacement_(vector)>
 
-# ## 1. import software
+# ## 1. Import Software
 
-using IndividualDisplacements, MeshArrays, OrdinaryDiffEq
-using Plots, Statistics, MITgcmTools, DataFrames
+using IndividualDisplacements, OrdinaryDiffEq
+using Plots, Statistics, DataFrames
 p=dirname(pathof(IndividualDisplacements))
 include(joinpath(p,"../examples/plot_Plots.jl"))
 
-# ## 2. Read gridded variables as `MeshArray`s
-
-dirIn="flt_example/"
-prec=Float32
-df=ReadDisplacements(dirIn,prec)
-uvetc=IndividualDisplacements.example2_setup()
-
-# ## 2. Recompute displacements from gridded flow fields
-
-# Define the ODE problem
-
-nSteps=2998
-tspan = (0.0,nSteps*3600.0)
-
-# Set up initial conditions
-#
-# _Note how the size of sol (ie nb of steps) depends on initial location:_
+# ## 2. Setup Problem
 
 # +
+uvetc=IndividualDisplacements.example2_setup()
+
 #ii1=1:10:80; ii2=1:10:42; #->sol is (2, 40, 40065)
 #ii1=30:37; ii2=16:20; #->sol is (2, 40, 9674)
 #ii1=10:17; ii2=16:20; #->sol is (2, 40, 51709)
 ii1=5:5:40; ii2=5:5:25; #->sol is (2, 40, 51709)
 
 n1=length(ii1); n2=length(ii2);
-uInitS=Array{Float64,2}(undef,(2,n1*n2))
+u0=Array{Float64,2}(undef,(2,n1*n2))
 for i1 in eachindex(ii1); for i2 in eachindex(ii2);
         i=i1+(i2-1)*n1
-        uInitS[1,i]=ii1[i1]-0.5
-        uInitS[2,i]=ii2[i2]-0.5
+        u0[1,i]=ii1[i1]-0.5
+        u0[2,i]=ii2[i2]-0.5
 end; end;
-
-prob = ODEProblem(⬡,uInitS,tspan,uvetc)
 # -
 
-# Compute solution
+# ## 3. Compute Trajectories
+#
+# - Define an ODE problem.
+# - Solve the ODE problem to compute trajectories.
 
-sol = solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
+𝑇 = (0.0,2998.0*3600.0)
+prob = ODEProblem(⬡,u0,𝑇,uvetc)
+
+sol = solve(prob,Tsit5(),reltol=1e-6,abstol=1e-6)
 size(sol)
 
-# Display results
+# ## 4. Display results
 
 # +
 ID=collect(1:size(sol,2))*ones(1,size(sol,3))
-lon=5000* mod.(sol[1,:,:],80)
-lat=5000* mod.(sol[2,:,:],42)
+lon=5000* mod.(sol[1,:,:],80); lat=5000* mod.(sol[2,:,:],42)
 df = DataFrame(ID=Int.(ID[:]), lon=lon[:], lat=lat[:])
 
 plt=PlotBasic(df,size(sol,2),100000.0)
 display(plt)
+# -
+
+df_from_mitgcm=ReadDisplacements("flt_example/",Float32)
+plt=PlotBasic(df_from_mitgcm,40,100000.0)
+display(plt)
+
+
