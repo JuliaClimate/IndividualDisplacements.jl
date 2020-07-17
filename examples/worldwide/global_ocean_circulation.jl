308b71a710775a36aaae7d2ba2671095dfe05056
gaelforget <gforget@mit.edu>
2020-07-17 11:15:42 -0400

    mv from jupytext to literate [3/n]
    Former-commit-id: 67a8a8739a31a7296b894557324adc90c33a6ed0

--- examples/worldwide/global_ocean_circulation.jl
+++ examples/worldwide/global_ocean_circulation.jl
@@ -1,35 +1,20 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,jl:light
-#     text_representation:
-#       extension: .jl
-#       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
-#   kernelspec:
-#     display_name: Julia 1.3.1
-#     language: julia
-#     name: julia-1.3
-# ---
-
 # # Global Ocean Particle Simulation Example
 #
 # Particles moving with climatological monthly mean flow at selected depth level (e.g. `k=10` for 95 m) based on an ocean state estimate (ECCO v4 r2 from https://ecco-group.org).
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# ### For More Documentation 
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/solid_body_rotation.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/solid_body_rotation.ipynb)
 #
-# - <https://en.wikipedia.org/wiki/Displacement_(vector)>
-# - <https://juliaclimate.github.io/IndividualDisplacements.jl/dev>
-# - <https://juliaclimate.github.io/MeshArrays.jl/dev>
-# - <https://docs.juliadiffeq.org/latest> 
+# ### For More Documentation
+# For additional documentation e.g. see :
+# [1](https://JuliaClimate.github.io/MeshArrays.jl/dev/),
+# [2](https://JuliaClimate.github.io/IndividualDisplacements.jl/dev/),
+# [3](https://docs.juliadiffeq.org/latest/solvers/ode_solve.html),
+# [4](https://en.wikipedia.org/wiki/Displacement_(vector))
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
 # ## 1. import software
 
-# +
 using IndividualDisplacements, MeshArrays, OrdinaryDiffEq
 using Statistics, MITgcmTools, DataFrames
 
@@ -37,10 +22,9 @@ p=dirname(pathof(IndividualDisplacements))
 include(joinpath(p,"../examples/helper_functions.jl"))
 get_grid_if_needed()
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
 # ## 2. Read gridded variables as `MeshArray`s
 
-# +
 k=10 #choice of vertical level
 ny=10 #number of simulated years (20 for k>20)
 r_reset = 0.01 #fraction of the particles reset per month (0.05 for k<=10)
@@ -53,10 +37,9 @@ r_reset = 0.01 #fraction of the particles reset per month (0.05 for k<=10)
 #initialize u0,u1 etc
 uvetc=read_uvetc(k,0.0,Γ,"nctiles_climatology/");
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
 # ## 3. Sample velocity & trajectory computations
 
-# +
 #initial condition
 uInit=[45.0,100.0,1.0]
 du=fill(0.0,3);
@@ -73,13 +56,13 @@ for i in eachindex(ii)
     u[i],v[i],f[i]=du
 end
 
-# + {"slideshow": {"slide_type": "subslide"}}
+#nb # %% {"slideshow": {"slide_type": "subslide"}}
 #using plots
 #plt=plot(u)
 #plot!(v)
 #display(plt)
 
-# + {"slideshow": {"slide_type": "subslide"}}
+#nb # %% {"slideshow": {"slide_type": "subslide"}}
 #Solve for single trajectory (uInit)
 𝑇 = (0.0,uvetc["t1"])
 prob = ODEProblem(⬡!,uInit,𝑇,uvetc)
@@ -87,21 +70,22 @@ sol_one = solve(prob,Tsit5(),reltol=1e-4,abstol=1e-4)
 sol_two = solve(prob,Euler(),dt=1e6)
 size(sol_one)
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # ## 3. Main Computation Loop
 #
 # - initial particle positions randomly over Global Ocean
 # - initial integration from time 0 to 0.5 month
 # - update velocity fields & repeat for n years
-# -
 
-# ## 3.1 Initialization & Initial Solution  
+# ## 3.1 Initialization & Initial Solution
+#
 
-# +
-#(u0,du)=initialize_gridded(uvetc,10);
+#(u0,du)=initialize_gridded(uvetc,10)
 
 #(lon, lat) = randn_lonlat(20000)
-#(u0,du)=initialize_lonlat(Γ,lon,lat; msk=Γ["hFacC"][:,k]);
+#(u0,du)=initialize_lonlat(Γ,lon,lat; msk=Γ["hFacC"][:,k])
+
+# Or
 
 lo0,lo1=(-160.0,-150.0)
 la0,la1=(35.0,45.0)
@@ -110,34 +94,33 @@ lon=lo0 .+(lo1-lo0).*rand(n)
 lat=la0 .+(la1-la0).*rand(n)
 (u0,du)=initialize_lonlat(Γ,lon,lat; msk=Γ["hFacC"][:,k]);
 
-# + {"slideshow": {"slide_type": "skip"}}
+#nb # %% {"slideshow": {"slide_type": "skip"}}
 u0_store = deepcopy(u0)
 n_store = size(u0_store,2)
 
-#r_reset = 0.05 #fraction of the particles reset per month
+# Fraction of the particles reset per month
+
+#r_reset = 0.05
 n_reset = Int(round(r_reset*n_store))
 #k_reset = rand(1:size(u0_store,2), n_reset)
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
 # Solve for all trajectories for first 1/2 month
-# -
 
 prob = ODEProblem(⬡!,u0,𝑇,uvetc)
 sol = solve(prob,Euler(),dt=uvetc["dt"]/8.0)
 size(sol)
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
 # Map `i,j` to `lon,lat` coordinates and convert to `DataFrame`
-# -
 
 df=postprocess_lonlat(sol,uvetc)
 df[1:4,:]
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # ## 3.2 Repeat `ny` Years x 12 Months
 #
 # _A fraction of the particles, randomly selected, is reset every month to maintain a relatively homogeneous coverage of the Global Ocean by the fleet of particles._
-# -
 
 t0=[uvetc["t1"]]
 u0 = deepcopy(sol[:,:,end])
@@ -164,20 +147,18 @@ for y=1:ny
     println(size(df))
 end
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # ## 4. Plot trajectories
 #
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# - Try `PlotBasic`, `PlotMapProj`, or `PlotMakie`
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+# Try `PlotBasic`, `PlotMapProj`, or `PlotMakie`
 
-# +
 #p=dirname(pathof(IndividualDisplacements))
 #nn=1000
 
 #include(joinpath(p,"../examples/recipes_plots.jl"))
 #plt=PlotBasic(df,nn,180.)
-#display(plt)
 
 #include(joinpath(p,"../examples/recipes_pyplot.jl"))
 #PyPlot.figure(); PlotMapProj(df,nn)
@@ -187,10 +168,10 @@ end
 #scene=PlotMakie(df,nn,180.0)
 ##Makie.save("LatLonCap300mDepth.png", scene)
 
-# + {"slideshow": {"slide_type": "skip"}, "cell_type": "markdown"}
-# - create `lon`, `lat`, and `DL` to use in plot background and ...
+#nb # %% {"slideshow": {"slide_type": "skip"}, "cell_type": "markdown"}
+# Or first create `lon`, `lat`, and `DL` to use in plot background:
 
-# + {"slideshow": {"slide_type": "skip"}}
+#nb # %% {"slideshow": {"slide_type": "skip"}}
 nf=size(u0,2)
 nt=size(df,1)/nf
 t=[ceil(i/nf)-1 for i in 1:nt*nf]
@@ -204,9 +185,8 @@ DL=log10.(Interpolate(Γ["Depth"],f,i,j,w))
 DL[findall((!isfinite).(DL))].=NaN
 DL=reshape(DL,size(lon));
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# - generate movie using `GeoMakie.jl`
-# -
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+# Generate movie using `GeoMakie.jl` (if `true`)
 
 if false
     using ArgoData
@@ -216,7 +196,9 @@ if false
     ProjScatterMovie(scene,df,tt,"GlobalDomain_fleet_k"*"$k"*"_v1.mp4",dt=1.0,mrksz=5e3)
 end
 
-# +
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+# Generate movie using `Plots.jl` (if `true`)
+
 if false
     using Plots
     dt=0.0001
@@ -234,6 +216,3 @@ if false
         xlims=(-180.0,180.0),ylims=(-90.0,90.0),marker = (:dot, stroke(0)))
 
 end
-# -
-
-
