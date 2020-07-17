0036086b7ea8851b64a22ca5ac596d4630034c59
gaelforget <gforget@mit.edu>
2020-07-17 08:25:06 -0400

    mv from jupytext to literate

--- examples/basics/solid_body_rotation.jl
+++ examples/basics/solid_body_rotation.jl
@@ -1,61 +1,47 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,jl:light
-#     text_representation:
-#       extension: .jl
-#       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
-#   kernelspec:
-#     display_name: Julia 1.3.1
-#     language: julia
-#     name: julia-1.3
-# ---
-
-# # Single Particle Simulation Example
+# # Single Particle Simulation
 #
-# In this example we simulate the trajectory of a particle in an idealized representation of an ocean eddy. To start, we use solid body rotation around a central point. As an exercise left to the user, directions are provided e.g. to add a convergence / divergence term. 
+# Simulate the trajectory of a particle in a perfectly circular flow (i.e.
+# _solid body rotation_), which may represent e.g. an ocean meso-scale eddy.
+#
+# As an exercise left to the user, directions are provided e.g. to add a convergence / divergence term.
 #
 # - 1. setup the software and initialize example
 # - 2. simulate trajectories & plot results
 # - 3. experiment with parameters (user)
-
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# ### For More Documentation 
 #
-# - <https://en.wikipedia.org/wiki/Displacement_(vector)>
-# - <https://juliaclimate.github.io/IndividualDisplacements.jl/dev>
-# - <https://juliaclimate.github.io/MeshArrays.jl/dev>
-# - <https://docs.juliadiffeq.org/latest>
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/solid_body_rotation.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/solid_body_rotation.ipynb)
+#
+# ### For More Documentation
+# For additional documentation e.g. see :
+# [1](https://JuliaClimate.github.io/MeshArrays.jl/dev/),
+# [2](https://JuliaClimate.github.io/IndividualDisplacements.jl/dev/),
+# [3](https://docs.juliadiffeq.org/latest/solvers/ode_solve.html),
+# [4](https://en.wikipedia.org/wiki/Displacement_(vector))
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
-# ## 1.1 Import Software, i.e. `Julia` Packages
-# -
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## 1.1 Import Software
 
 using OrdinaryDiffEq, Plots
 using IndividualDisplacements, MeshArrays
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# ## 1.2  Gridded Domain Example
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+# ## 1.2  Gridded Domain
 #
 # - define `SetPeriodicDomain` function, which uses `MeshArrays.jl`
 # - call `SetPeriodicDomain` function with a chosen grid size; e.g. `np=16`
 
-# +
 np=16
 
 Γ=simple_periodic_domain(np);
 
-# + {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
-# ## 1.3 Define Time Period & Velocity Field
+#nb # %% {"slideshow": {"slide_type": "subslide"}, "cell_type": "markdown"}
+# ## 1.3 Time & Velocity Fields
 #
 # - define time range
 # - define velocity field(s)
 # - store in `𝑃` (dictionary) with grid variables
 
-# + {"slideshow": {"slide_type": "skip"}}
 #time range
 t0=0.0
 t1=0.95*2*pi
@@ -67,7 +53,7 @@ u=-(Γ["YG"].-Γ["YG"][1][i,i])
 v=(Γ["XG"].-Γ["XG"][1][i,i])
 
 #add some convergence to / divergence from central location
-d=0.0 
+d=0.0
 #d=-0.10
 u=u+d*(Γ["XG"].-Γ["XG"][1][i,i])
 v=v+d*(Γ["YG"].-Γ["YG"][1][i,i])
@@ -76,53 +62,51 @@ v=v+d*(Γ["YG"].-Γ["YG"][1][i,i])
 𝑃=Dict("u0" => u, "u1" => u, "v0" => v, "v1" => v, "t0" => t0, "t1" => t1)
 𝑃=merge(𝑃,Γ);
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
-# ## 1.4 Define initial position and time period
-# -
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## 1.4 Initial Position and Time
 
 u0=np*[1/3,1/3]
 du=fill(0.0,2)
 𝑇 = (𝑃["t0"],𝑃["t1"]);
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
-# ## 2.1 solve for particle trajectory
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## 2.1 Solve For Particle Trajectory
 #
 # - `ODEProblem` formulates the differential equation along with the time period `𝑇`, parameters `𝑃`
 # - `solve` then performs the integration over `𝑇`, starting from `u0`
 #
-# _Try `?ODEProblem` or `?solve` for additional documentation_
+# _For additional documentation, try `?ODEProblem` or `?solve`_
 
-# +
 prob = ODEProblem(⬡,u0,𝑇,𝑃)
 sol = solve(prob,Tsit5(),reltol=1e-8)
 
 x,y=sol[1,:],sol[2,:]
 nt=length(x)
 
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
-# ## 2.2 visualize particle trajectory
+#nb # %% {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## 2.2 Visualize Particle Trajectory
 #
 # - define `myplot` convenience function
 # - generate animation using `myplot`
-#
-# _Want a single plot? try uncommenting one line at a time in the next cell below_
+# - single plot example using `myplot`
 
-# + {"slideshow": {"slide_type": "-"}}
 myplot(i)=plot(x[1:i],y[1:i],linewidth=2,arrow = 2,
     title="Solid body rotation / Spiral example",leg=false,
     xaxis="x",yaxis="y",xlims=(0,np),ylims=(0,np))
 
+#nb # %% {"slideshow": {"slide_type": "subslide"}}
+# Animation example:
 
-# + {"slideshow": {"slide_type": "subslide"}}
 p=Int(ceil(nt/100))
 anim = @animate for i ∈ 1:p:nt
     myplot(i)
 end
 pth=tempdir()*"/"
 gif(anim, pth*"SolidBodyRotation.gif", fps = 15)
-# + {"slideshow": {"slide_type": "subslide"}}
+
+#nb # %% {"slideshow": {"slide_type": "subslide"}}
+# Single plot example:
+
 plt=myplot(nt)
 scatter!(plt,[u0[1]],[u0[2]])
-savefig(plt,pth*"SolidBodyRotation.png")
-# -
-
+#savefig(plt,pth*"SolidBodyRotation.png")
