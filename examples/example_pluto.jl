693e39c3efc7d11d160c4c5b25abb6294bd6cca6
gaelforget <gforget@mit.edu>
2020-08-26 07:26:36 -0400

    add plots & improve pluto-based example
    Former-commit-id: f87f97bafab7b68a77380432cfb7937968dce1cc [formerly e40e2dbcd464a89d7b5760f876ee2d52e21417e3]
    Former-commit-id: f07bd7b3a1e307e4fb66bb0421429b0cef0796e8
    Former-commit-id: 4d9450a399c2e0c6ab7b1ddfa97092b2fe7174e0

--- examples/example_pluto.jl
+++ examples/example_pluto.jl
@@ -15,7 +15,8 @@ end
 
 # ╔═╡ 747d446a-dfeb-11ea-3533-c9404fd41688
 begin
-	using IndividualDisplacements, Plots, NetCDF, OrdinaryDiffEq, DataFrames, Statistics, StatsPlots
+	using IndividualDisplacements, Plots, NetCDF, OrdinaryDiffEq, DataFrames
+	using Statistics, StatsPlots, PlutoUI
 
 	p=dirname(pathof(IndividualDisplacements))
     include(joinpath(p,"../examples/example123.jl"))
@@ -24,23 +25,28 @@ begin
 	𝑃,Γ=OCCA_setup()	
 	tmp=(Γ = Γ, m = "OCCA")
     𝑃=merge(𝑃,tmp)
-	✓ = "😃" 
+	✓ = "😃"
+	"$✓ Set up packages, gridded domain, etc"
 end
 
-# ╔═╡ 09f32060-dff7-11ea-1276-add3ff402f18
-@bind lon0 html"<input type=range min=-180.0 max=180.0>"
+# ╔═╡ bf19d29c-e70e-11ea-0153-d3a49981d56c
+md"""longitude,latitude of South West corner =
+$(@bind lon0 NumberField(-180.0:10:180.0; default=0.0))
+,
+$(@bind lat0 NumberField(-90.0:5:90.0; default=-50.0))
+"""
 
-# ╔═╡ 3850bd7c-dff8-11ea-3a9b-1ba4c812e614
-@bind lat0 html"<input type=range min=-90.0 max=90.0>"
+# ╔═╡ 4935fd46-e70f-11ea-386c-f9c444a20644
+md"""depth level (integer), set size (nb of particles) =
+$(@bind klev NumberField(1:10; default=5))
+,
+$(@bind npar NumberField(50:50:1000; default=100))
+""" 
 
-# ╔═╡ d79a0800-dffa-11ea-1751-9de46bd95c3c
-@bind npar html"<input type=range min=0 max=1000>"
-
-# ╔═╡ 3039d8a0-dffb-11ea-33a2-03f957d4efff
-@bind klev html"<input type=range min=0 max=10>"
-
-# ╔═╡ f1da7264-e675-11ea-0f7e-ef6ff63c9123
-@bind nt html"<input type=range min=2 max=75>"
+# ╔═╡ 9c80e722-e70f-11ea-22a6-0be2e85f3b8b
+md"""time step (for plotting)= 
+$(@bind tt Slider(2:75; default=50, show_value=true))
+"""
 
 # ╔═╡ e25eee9e-dfee-11ea-2a4c-3946ccb63876
 begin
@@ -48,7 +54,7 @@ begin
 	la0=lat0; la1=la0+5
 	z_init=klev
 	n_part=npar
-	(particles=n_part,longitudes=(lo0,lo1),latitudes=(la0,la1),level=z_init)
+	(particles=n_part,longitudes=(lo0,lo1),latitudes=(la0,la1),level=z_init,plot_time=tt)
 end
 
 # ╔═╡ f75fae30-dfee-11ea-18ef-259321acfa2f
@@ -83,45 +89,46 @@ begin
 	  return df
 	end
 	
-	✓
+	"$✓ Set up Individuals"
 end
 
 # ╔═╡ 9ffe84c0-dff0-11ea-2726-8924892df73a
 begin
-   𝐼 = Individuals{Float64}(📌=xy, 🔴=tr, 🆔=id, 🚄 = dxyz_dt, ∫ = solv, 🔧 = postproc, 𝑃=𝑃)
-   𝑇=(0.0,𝐼.𝑃.𝑇[2])
-   ∫!(𝐼,𝑇)
-end
-
-# ╔═╡ e59d725e-e61f-11ea-255f-0b5550795ad9
-begin
+	𝐼 = Individuals{Float64}(📌=deepcopy(xy), 🔴=deepcopy(tr), 🆔=id, 
+		                     🚄 = dxyz_dt, ∫ = solv, 🔧 = postproc, 𝑃=𝑃)
+	
+	𝑇=(0.0,𝐼.𝑃.𝑇[2])
+	∫!(𝐼,𝑇)
+	
 	🔴_by_ID = groupby(𝐼.🔴, :ID)
 	🔴_by_t = groupby(𝐼.🔴, :t)
 	
 	ff(x) = x[1]
-	tmp1=combine(🔴_by_ID,:lon => ff => :ini_lon,:lat => ff => :ini_lat)
-	𝐼.🔴.dlon=𝐼.🔴.lon - tmp1.ini_lon[𝐼.🔴.ID]
-	𝐼.🔴.dlat=𝐼.🔴.lat - tmp1.ini_lat[𝐼.🔴.ID]
+	tmp1=combine(🔴_by_ID,:lon => ff => :lo,:lat => ff => :la)
+	𝐼.🔴.dlon=𝐼.🔴.lon - tmp1.lo[𝐼.🔴.ID]
+	𝐼.🔴.dlat=𝐼.🔴.lat - tmp1.la[𝐼.🔴.ID]
+	
+	nt=length(unique(𝐼.🔴.t))
 
-#	df_a = DataFrame(i=1:10, x=0.1:0.1:1.0, y='a':'j');
-#	nunique(x)=length(unique(x))
-#	describe(df_a,:nunique => nunique)
+	"$✓ ∫!(𝐼,𝑇) etc"
 end
 
 # ╔═╡ f65ddffa-e63a-11ea-34a6-2fa9284e98fa
 begin
 	mx=50.0
-	f(x)=x[nt]-x[1]
-	g(x)=x[nt]
+
+	#f(x)=x[tt]-x[1]
 	#f(x)=last(x).-first(x)
 	#cdf = combine(🔴_by_ID,nrow,:lat => f => :dlat,:lon => f => :dlon)
-	cdf = combine(🔴_by_ID,:dlat => g => :dlat,:dlon => g => :dlon)
-	histogram2d(cdf.dlon,cdf.dlat,nbins = (10, 10) ,xlims=(-mx,mx),ylims=(-mx,mx))
-	#scatter(cdf.dlon,cdf.dlat,xlims=(-mx,mx),ylims=(-mx,mx))
-end
 
-# ╔═╡ 6a1a6d38-e6d5-11ea-1182-f7e690710745
-describe(🔴_by_ID[2])
+	g(x)=x[tt]
+	cdf = combine(🔴_by_ID,:dlat => g => :dlat,:dlon => g => :dlon)
+	
+	plt_hist=histogram2d(cdf.dlon,cdf.dlat,nbins = (10, 10),
+		xlims=(-mx,mx),ylims=(-mx,mx), colorbar=false)
+	#scatter(cdf.dlon,cdf.dlat,xlims=(-mx,mx),ylims=(-mx,mx))
+	"$✓ dlon, dlat histogram2d"
+end
 
 # ╔═╡ 7d52252e-e006-11ea-2632-df2af831b52f
 begin
@@ -132,25 +139,15 @@ begin
 	
 	𝐶(g::ColorGradient) = RGB[g[z] for z=LinRange(0,1,length(🔴_by_t))]
 	𝐶(t::Int) = 𝐶(cgrad(:inferno))[t]	
-	✓
-end
-
-# ╔═╡ a13d6ea6-dff1-11ea-0713-cb235e28cf79
-begin
-	plt=contourf(x,y,z,clims=(-.5,4.),c = :ice, 
-		colorbar=false, xlims=(-180.0,180.0),ylims=(-90.0,90.0))
-	scatter!(plt,🔴_by_t[1].lon,🔴_by_t[1].lat,c=:gold,leg=:none,
-		markersize=2.0, marker = (:dot, stroke(0)) )
-	scatter!(plt,🔴_by_t[nt].lon,🔴_by_t[nt].lat,c=:red,leg=:none,
-		markersize=2.0, marker = (:dot, stroke(0)) )
+	"$✓ Set up plotting"
 end
 
 # ╔═╡ 0b12cf52-e6e3-11ea-1a01-dd0c49c9e641
 begin
-	plt2 = @df 🔴_by_t[1] density(:dlat, leg = :none, colour = 𝐶(1), ylims=(0,0.5))
-	[@df 🔴_by_t[tt] density!(plt2,:dlat, leg = :none, colour = 𝐶(tt)) for tt in 2:length(🔴_by_t)];
-	density!(plt2,🔴_by_t[nt].dlat, leg = :none, colour = :cyan, linewidth=4)
-	plt2
+	plt_dlat = @df 🔴_by_t[1] density(:dlat, leg = :none, colour = 𝐶(1), ylims=(0,0.5))
+	[@df 🔴_by_t[tt] density!(plt_dlat,:dlat, leg = :none, colour = 𝐶(tt)) for tt in 2:length(🔴_by_t)];
+	density!(plt_dlat,🔴_by_t[nt].dlat, leg = :none, colour = :cyan, linewidth=4)
+	"$✓ dlat density"
 end
 
 # ╔═╡ 6f70033a-e6cc-11ea-373e-6dcbaaa53d15
@@ -159,23 +156,31 @@ begin
 	#@df 𝐼.🔴 density(:lon, group = (:t), leg = :none, palette = cgrad(:ice))
 	plt_dlon = @df 🔴_by_t[1] density(:dlon, leg = :none, colour = 𝐶(1), ylims=(0,0.5))
 	[@df 🔴_by_t[tt] density!(plt_dlon,:dlon, leg = :none, colour = 𝐶(tt)) for tt in 2:length(🔴_by_t)];
-	density!(plt_dlon,🔴_by_t[nt].dlon, leg = :none, colour = :cyan, linewidth=4)
-	plt_dlon
+	density!(plt_dlon,🔴_by_t[tt].dlon, leg = :none, colour = :cyan, linewidth=4)
+	"$✓ dlon density"
+end
+
+# ╔═╡ a13d6ea6-dff1-11ea-0713-cb235e28cf79
+begin
+	plt_map=contourf(x,y,z,clims=(-.5,4.),c = :ice, 
+		colorbar=false, xlims=(-180.0,180.0),ylims=(-90.0,90.0))
+	scatter!(plt_map,🔴_by_t[1].lon,🔴_by_t[1].lat,c=:gold,leg=:none,
+		markersize=2.0, marker = (:dot, stroke(0)) )
+	scatter!(plt_map,🔴_by_t[tt].lon,🔴_by_t[tt].lat,c=:red,leg=:none,
+		markersize=2.0, marker = (:dot, stroke(0)) )
+	
+	plot(plt_map,plt_hist,plt_dlon,plt_dlat)
 end
 
 # ╔═╡ Cell order:
-# ╠═09f32060-dff7-11ea-1276-add3ff402f18
-# ╠═3850bd7c-dff8-11ea-3a9b-1ba4c812e614
-# ╠═d79a0800-dffa-11ea-1751-9de46bd95c3c
-# ╠═3039d8a0-dffb-11ea-33a2-03f957d4efff
-# ╠═f1da7264-e675-11ea-0f7e-ef6ff63c9123
+# ╟─bf19d29c-e70e-11ea-0153-d3a49981d56c
+# ╟─4935fd46-e70f-11ea-386c-f9c444a20644
+# ╟─9c80e722-e70f-11ea-22a6-0be2e85f3b8b
 # ╟─e25eee9e-dfee-11ea-2a4c-3946ccb63876
 # ╟─a13d6ea6-dff1-11ea-0713-cb235e28cf79
 # ╟─f65ddffa-e63a-11ea-34a6-2fa9284e98fa
 # ╟─0b12cf52-e6e3-11ea-1a01-dd0c49c9e641
 # ╟─6f70033a-e6cc-11ea-373e-6dcbaaa53d15
-# ╠═6a1a6d38-e6d5-11ea-1182-f7e690710745
-# ╟─e59d725e-e61f-11ea-255f-0b5550795ad9
 # ╟─9ffe84c0-dff0-11ea-2726-8924892df73a
 # ╟─7d52252e-e006-11ea-2632-df2af831b52f
 # ╟─f75fae30-dfee-11ea-18ef-259321acfa2f
