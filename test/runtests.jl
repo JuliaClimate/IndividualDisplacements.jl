261994626cb12e854d4c1a85862ee55a6fca4d80
gaelforget <gforget@mit.edu>
2020-09-25 12:12:00 -0400

    fix get_from_dataverse call (?) [2/n]
    Former-commit-id: dcaf9a828bc642e72a8a631ec54fa181f98ece54

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Test, Documenter
-using IndividualDisplacements, MeshArrays, OrdinaryDiffEq
-include("helper_functions.jl")
+using IndividualDisplacements, MeshArrays, OrdinaryDiffEq, OceanStateEstimation
+
+include(joinpath(dirname(pathof(IndividualDisplacements)),"../test/helper_functions.jl"))
+include(joinpath(dirname(pathof(IndividualDisplacements)),"../examples/helper_functions.jl"))
+get_llc90_grid_if_needed(); get_ecco_velocity_if_needed();
+get_ll360_grid_if_needed(); get_occa_velocity_if_needed();
+get_flt_ex_if_needed();
 
 @testset "test1" begin
     uvetc,sol=test1_setup()
