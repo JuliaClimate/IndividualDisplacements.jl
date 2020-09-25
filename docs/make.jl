f18911a6eb2aa83483e3280b3c915f92bca710fe
gaelforget <gforget@mit.edu>
2020-09-25 12:54:24 -0400

    dont compile all notebooks
    Former-commit-id: a35f79610937ed12f27539e4ed9ea711bdb55182

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ execute = true # Set to true for executing notebooks and documenter!
 nb = true      # Set to true to generate the notebooks
 
 lst1 = ["solid_body_rotation","random_flow_field","global_ocean_circulation","three_dimensional_ocean","detailed_look","particle_cloud"]
-lst2 = ["solid_body_rotation","random_flow_field","global_ocean_circulation","three_dimensional_ocean","detailed_look","particle_cloud"]
+lst2 = ["solid_body_rotation","random_flow_field","detailed_look","particle_cloud"]
 tst1(x) = Bool(sum(isequal.(x, lst1)))
 tst2(x) = Bool(sum(isequal.(x, lst2)))
 
