696bf4cfd87fe5fcbab4f5d03f74d8fbc2c76f95
gaelforget <gforget@mit.edu>
2021-10-04 15:17:39 -0400

    Artifact > lazy, add ci.yml, + related updates

--- examples/basics/detailed_look.jl
+++ examples/basics/detailed_look.jl
@@ -31,7 +31,7 @@ include(joinpath(p,"../examples/helper_functions.jl"));
 #
 # from `MITgcm/pkg/flt`
 
-dirIn=IndividualDisplacements.flt_example
+dirIn=IndividualDisplacements.flt_example_path
 prec=Float32
 df=read_flt(dirIn,prec);
 
