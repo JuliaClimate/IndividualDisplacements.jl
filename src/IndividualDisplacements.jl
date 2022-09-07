90a1599b4958dcd118033bc96758e012b442aa74
gaelforget <gforget@mit.edu>
2022-09-07 09:34:28 -0400

    mv flt_example_download etc to src/various.jl

--- src/IndividualDisplacements.jl
+++ src/IndividualDisplacements.jl
@@ -1,28 +1,13 @@
 module IndividualDisplacements
 
 using MeshArrays, CyclicArrays, OrdinaryDiffEq, DataFrames
-using NetCDF, Dates, CFTime, CSV, UnPack, Random, Artifacts, LazyArtifacts
-
-p=dirname(pathof(IndividualDisplacements))
-artifact_toml = joinpath(p, "../Artifacts.toml")
-flt_example_hash = artifact_hash("flt_example", artifact_toml)
-flt_example_path = joinpath(artifact_path(flt_example_hash)*"/","flt_example-1.0/")
-flt_example_download() = artifact"flt_example"
-
-#needed within OrdinaryDiffEq somehow -- begin
-import Base: zero
-zero(tmp::Array) = zero.(tmp)
-
-import OrdinaryDiffEq.DiffEqBase: abs2_and_sum, UNITLESS_ABS2
-abs2_and_sum(x::Matrix{Vector{Float64}},y::Matrix{Vector{Float64}}) = 
-    reduce(Base.add_sum,x,init=0*x[1]) .+ reduce(Base.add_sum,y,init=0*x[1])
-@inline UNITLESS_ABS2(x::Matrix{Vector{Float64}}) = mapreduce(UNITLESS_ABS2,abs2_and_sum, x, init=0*x[1])
-#needed within OrdinaryDiffEq somehow -- end
+using Dates, CFTime, CSV, Random, Artifacts, LazyArtifacts
 
 include("API.jl")
 include("compute.jl")
 include("data_wrangling.jl")
 include("toy_models.jl")
+include("various.jl")
 
 export Individuals, ∫!
 export FlowFields, convert_to_FlowFields
