310fb761a042e4549e3fd02a81a6436c62fb994e
gaelforget <gforget@mit.edu>
2024-06-01 08:43:40 -0400

    add record and groupby methods

--- src/IndividualDisplacements.jl
+++ src/IndividualDisplacements.jl
@@ -10,10 +10,11 @@ include("toy_models.jl")
 include("various.jl")
 
 DiffEqBase.solve!(𝐼::Individuals,args...)=∫!(𝐼::Individuals,args...)
-
+DataFrames.groupby(I::Individuals,args...) = groupby(I.🔴,args...)
 F_Array3D=𝐹_Array3D; F_Array2D=𝐹_Array2D; F_MeshArray3D=𝐹_MeshArray3D; F_MeshArray2D=𝐹_MeshArray2D
+record(I::Individuals) = I.🔴
 
-export Individuals, ∫!, solve!
+export Individuals, ∫!, solve!, record, groupby
 export FlowFields, convert_to_FlowFields
 export 𝐹_Array3D, 𝐹_Array2D, 𝐹_MeshArray3D, 𝐹_MeshArray2D
 export F_Array3D, F_Array2D, F_MeshArray3D, F_MeshArray2D
@@ -23,6 +24,5 @@ export nearest_to_xy, randn_lonlat, interp_to_lonlat
 export gcdist, stproj, stproj_inv
 
 export random_flow_field, vortex_flow_field
-
 
 end # module
