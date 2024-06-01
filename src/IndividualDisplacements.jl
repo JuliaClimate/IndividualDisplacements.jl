43cd0c623eff2b99b6b012921d629a7b484fc5d3
gaelforget <gforget@mit.edu>
2024-06-01 08:06:36 -0400

    new aliases to avoid unicode

--- src/IndividualDisplacements.jl
+++ src/IndividualDisplacements.jl
@@ -9,14 +9,20 @@ include("data_wrangling.jl")
 include("toy_models.jl")
 include("various.jl")
 
-export Individuals, ∫!
+DiffEqBase.solve!(𝐼::Individuals,args...)=∫!(𝐼::Individuals,args...)
+
+F_Array3D=𝐹_Array3D; F_Array2D=𝐹_Array2D; F_MeshArray3D=𝐹_MeshArray3D; F_MeshArray2D=𝐹_MeshArray2D
+
+export Individuals, ∫!, solve!
 export FlowFields, convert_to_FlowFields
 export 𝐹_Array3D, 𝐹_Array2D, 𝐹_MeshArray3D, 𝐹_MeshArray2D
+export F_Array3D, F_Array2D, F_MeshArray3D, F_MeshArray2D
 export dxdt!, dxy_dt_CyclicArray, dxy_dt_replay
 export postprocess_MeshArray, add_lonlat!, postprocess_xy, interp_to_xy
 export nearest_to_xy, randn_lonlat, interp_to_lonlat
 export gcdist, stproj, stproj_inv
 
 export random_flow_field, vortex_flow_field
+
 
 end # module
