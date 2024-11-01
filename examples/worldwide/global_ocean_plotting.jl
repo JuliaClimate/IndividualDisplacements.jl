19472a968e33041ed877d7070bf9d04b261ca4dd
gaelforget <gforget@mit.edu>
2024-11-01 03:17:36 -0400

    update plotting

--- examples/worldwide/global_ocean_plotting.jl
+++ examples/worldwide/global_ocean_plotting.jl
@@ -2,17 +2,18 @@
 module PlottingFunctions 
 
 using GLMakie, DataFrames, FileIO, Colors
+using DataDeps, MeshArrays
+
+lon180(x)=Float64(x>180.0 ? x-360.0 : x)
+lon360(x)=Float64(x<0.0 ? x+360.0 : x)
 
 function background()
     dx=0.1
-    path_img="inputs"
-    earth_img=load(joinpath(path_img,
-             "Blue_Marble_Next_Generation_topography_bathymetry.jpg"))
-    earth_img=reverse(permutedims(earth_img),dims=2)
+    lon,lat,basemap=demo.get_basemap()
     fig = Figure(size = (1200, 800), backgroundcolor = :grey80)
     ax = Axis(fig[1, 1])
-    image!(ax,-179.95:dx:179.95,-89.95:dx:89.95,earth_img)
-    hidedecorations!(ax)
+    im=image!(ax,lon[1,1]..lon[end,1],lat[1,1]..lat[1,end],basemap)
+    #hidedecorations!(ax)
     fig,ax
 end
 
@@ -31,9 +32,14 @@ include("global_ocean_plotting.jl")
 fil=joinpath("inputs","GulfStream_21_27.csv")
 df=CSV.read(fil,DataFrame)
 
-nt=length(unique(df.t)); xlims=(-85.0,5.0); ylims=(20.0,67.0)
-fig,tt=PlottingFunctions.plot([],df,xlims=xlims,ylims=ylims,
-	colormap=:linear_wcmr_100_45_c42_n256,colorrange=(-1300,00), add_colorbar=false)
+nt=length(unique(df.t))
+#xlims=(-85.0,5.0); ylims=(20.0,67.0)
+xlims=(0.0,360.0); ylims=(-80.0,90.0)
+cm=:linear_wcmr_100_45_c42_n256
+cr=(-1300,00)
+
+fig,tt=PlottingFunctions.plot([],df,colormap=cm,colorrange=cr)
+    #xlims=xlims,ylims=ylims,
 fig
 
 file_output_mp4=tempname()*".mp4"
@@ -42,7 +48,7 @@ PlottingFunctions.record(fig, file_output_mp4, -50:nt, framerate = 25) do t
 end
 ```
 """
-function plot(𝐼,🔴;time=0,xlims=(-180.0,180.0),ylims=(-90.0,90.0),
+function plot(𝐼,🔴;time=0,xlims=(0.0,360.0),ylims=(-80.0,90.0),
     colormap=:linear_wcmr_100_45_c42_n256,colorrange=(-1300,00),
     add_colorbar=false)
 
@@ -64,7 +70,7 @@ function plot(𝐼,🔴;time=0,xlims=(-180.0,180.0),ylims=(-90.0,90.0),
     scatter!(ax,lon_t1,lat_t1,markersize=1.0,color=:lightblue)
     for tx in -12:0 
         ttt=@lift(max(1,$tt+tx))
-        lon_tt=@lift(tmp1[$ttt][jj,:lon])
+        lon_tt=@lift(lon360.(tmp1[$ttt][jj,:lon]))
         lat_tt=@lift(tmp1[$ttt][jj,:lat])
         d_tt=@lift(max.(tmp1[$ttt][jj,:d],Ref(-1200)))
         scatter!(ax,lon_tt,lat_tt,markersize=4.0,
